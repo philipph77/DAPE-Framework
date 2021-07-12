719d048b786b1e4688abaeeb5e39c5fccf2e0374
Philipp Hallgarten <philipp.hallgarten@web.de>
2021-07-12 17:29:04 +0200

    mini bug fix

--- multi_source_framework.py
+++ multi_source_framework.py
@@ -15,10 +15,11 @@ import datasets
 
 
 class Framework(nn.Module):
-    def __init__(self, encoders, latent_dim, num_classes):
+    def __init__(self, encoders, latent_dim, num_classes, use_adversary=False):
         super().__init__()
         self.num_datasources = len(encoders)
         self.is_trained = False
+        self.use_adversary = False
         self.building_blocks = nn.ModuleList()
         self.building_blocks.extend(encoders)
         self.building_blocks.append(architectures.DenseClassifier(latent_dim, num_classes))
@@ -40,13 +41,14 @@ class Framework(nn.Module):
         return y_pred
 
     def add_datasource(self, encoder, x, d):
+        # ATTENTION: THIS IS NOT SUPPORTED YET!
         if not(self.is_trained):
             print("The Framework was not trained yet, you can add the data-source be training the whole framework")
             return
         for param in self.parameters():
             param.requires_grad = False
         self.building_blocks.insert(-1,encoder)
-        #TODO: insert retrain here
+        #TODO: insert training procedure for the encoder here
         pass
 
 def train(model, datasource_files, max_epochs=500, batch_size=64):
@@ -95,11 +97,15 @@ if __name__ == '__main__':
     import platform
     from sklearn.model_selection import train_test_split
     import helper_funcs
-    C = 32
-    T = 2*128
-    F1 = 32
-    D = 16
-    F2 = 8
+
+    if  platform.system() == 'Darwin':
+        path = '../../Datasets/private_encs/'
+    else:
+        path = '../Datasets/private_encs/'
+    datasource_files = [os.path.join(path,f) for f in os.listdir(path) if f.endswith('.npz') and 'split' in f and not('test' in f)]
+
+    batch_size = 64
+    F1, D, F2 = 32, 16, 8
 
     encoders = [
         architectures.EEGNetEncoder(channels=62, temporal_filters=F1, spatial_filters=D, pointwise_filters=F2, dropout_propability=0.25, latent_dim=20), #SEED
@@ -107,24 +113,13 @@ if __name__ == '__main__':
         architectures.EEGNetEncoder(channels=32, temporal_filters=F1, spatial_filters=D, pointwise_filters=F2, dropout_propability=0.25, latent_dim=20), #DEAP
         architectures.EEGNetEncoder(channels=14, temporal_filters=F1, spatial_filters=D, pointwise_filters=F2, dropout_propability=0.25, latent_dim=20)  #DREAMER
     ]
-
-    batch_size = 64
-    num_batches = 1000
-
     model = Framework(encoders, 20, 3)
-    print("----- Model Parameters -----")
+
+    print("----- #Model Parameters: -----")
     print(helper_funcs.count_parameters(model))
     print("---------")
+    #summary(model, input_size=[(1,62,2*256),(batch_size,1,62,2*256),(batch_size,1,32,2*128),(batch_size,1,14,2*128)])
+    #print(model)
 
-    
-    #summary(model, input_size=[(batch_size*num_batches,1,C,T),(batch_size*num_batches,1,C,T),(batch_size*num_batches,1,C,T),(batch_size*num_batches,1,C,T)])
-
-    if  platform.system() == 'Darwin':
-        path = '../../Datasets/private_encs/'
-    else:
-        path = '../Datasets/private_encs/'
-
-    datasource_files = [os.path.join(path,f) for f in os.listdir(path) if f.endswith('.npz') and 'split' in f and not('test' in f)]
-
-    #train(model, datasource_files)
+    train(model, datasource_files)
     
