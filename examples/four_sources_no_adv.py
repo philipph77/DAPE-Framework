57b1a9cbbcfd59b3df9a4987e118f84d7a84c9e0
Philipp Hallgarten <philipp.hallgarten@web.de>
2021-07-14 17:32:47 +0200

    new example

---
+++ examples/four_sources_no_adv.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.append("../eeg_private_encoders")
+from multi_source_framework import Framework
+import architectures
+import datasets
+from pipeline_funcs import train, train_adversarial
+from torch.utils.data import DataLoader
+import platform
+import os
+
+def main():
+    if  platform.system() == 'Darwin':
+        path = '../../Datasets/private_encs/'
+    else:
+        path = '../Datasets/private_encs/'
+    
+    train_datasource_files = [os.path.join(path,'train',f) for f in sorted(os.listdir(os.path.join(path, 'train'))) if f.endswith('.npz') and not('test' in f)]
+    validation_datasource_files = [os.path.join(path,'val', f) for f in sorted(os.listdir(os.path.join(path, 'val'))) if f.endswith('.npz') and not('test' in f)]
+
+    latent_dim = 2000
+    encoders = [
+        architectures.EEGNetEncoder(channels=32, temporal_filters=F1, spatial_filters=D, pointwise_filters=F2, dropout_propability=0.25, latent_dim=latent_dim, use_constrained_conv=False), #DEAP
+        architectures.EEGNetEncoder(channels=14, temporal_filters=F1, spatial_filters=D, pointwise_filters=F2, dropout_propability=0.25, latent_dim=latent_dim, use_constrained_conv=False),  #DREAMER
+        architectures.EEGNetEncoder(channels=62, temporal_filters=F1, spatial_filters=D, pointwise_filters=F2, dropout_propability=0.25, latent_dim=latent_dim, use_constrained_conv=False), #SEED
+        architectures.EEGNetEncoder(channels=62, temporal_filters=F1, spatial_filters=D, pointwise_filters=F2, dropout_propability=0.25, latent_dim=latent_dim, use_constrained_conv=False) #SEED-IV
+    ]
+    
+    training_data = datasets.MultiSourceDataset(train_datasource_files)
+    validation_data = datasets.MultiSourceDataset(validation_datasource_files)
+
+    train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True, num_workers=4, pin_memory=True)
+    validation_dataloader = DataLoader(validation_data, batch_size=64, shuffle=False, num_workers=4, pin_memory=True)
+
+    model = Framework(encoders, latent_dim, 3, use_adversary=False)
+    train(model, train_dataloader, validation_dataloader, 'seed-seed_iv-DCN-2000', '../logs/', max_epochs=100)
+
+    #model_adv = Framework(encoders, latent_dim, 3, use_adversary=True)
+    #train_adversarial(model_adv, train_dataloader, validation_dataloader, 'delete-seed-seed_iv-DCN-2000-adv', '../logs/',  0.05, max_epochs=500)      
+
+if __name__ == '__main__':
+    main()
