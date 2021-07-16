d9aaa432eb74140fccba024100834bc1934d8a78
Philipp Hallgarten <philipp.hallgarten@web.de>
2021-07-16 08:47:47 +0200

    renamed example files

--- examples/four_sources.py
+++ examples/four_sources.py
@@ -8,7 +8,7 @@ from torch.utils.data import DataLoader
 import platform
 import os
 
-def main():
+def run_four_sources():
     if  platform.system() == 'Darwin':
         path = '../../Datasets/private_encs/'
     else:
@@ -16,13 +16,14 @@ def main():
     
     train_datasource_files = [os.path.join(path,'train',f) for f in sorted(os.listdir(os.path.join(path, 'train'))) if f.endswith('.npz') and not('test' in f)]
     validation_datasource_files = [os.path.join(path,'val', f) for f in sorted(os.listdir(os.path.join(path, 'val'))) if f.endswith('.npz') and not('test' in f)]
-    train_datasource_files = [f for f in train_datasource_files if 'SEED' in f]
-    print(train_datasource_files)
-    validation_datasource_files = [f for f in validation_datasource_files if 'SEED' in f]
 
     latent_dim = 2000
-    encoders = [architectures.DeepConvNetEncoder(channels=62, latent_dim=latent_dim),
-                architectures.DeepConvNetEncoder(channels=62, latent_dim=latent_dim)]
+    encoders = [
+        architectures.DeepConvNetEncoder(channels=32, latent_dim=latent_dim), #DEAP
+        architectures.DeepConvNetEncoder(channels=14, latent_dim=latent_dim), #DREAMER
+        architectures.DeepConvNetEncoder(channels=62, latent_dim=latent_dim), #SEED
+        architectures.DeepConvNetEncoder(channels=62, latent_dim=latent_dim) #SEED-IV
+    ]
     
     training_data = datasets.MultiSourceDataset(train_datasource_files)
     validation_data = datasets.MultiSourceDataset(validation_datasource_files)
@@ -31,10 +32,10 @@ def main():
     validation_dataloader = DataLoader(validation_data, batch_size=64, shuffle=False, num_workers=4, pin_memory=True)
 
     model = Framework(encoders, latent_dim, 3, use_adversary=False)
-    #train(model, train_dataloader, validation_dataloader, 'seed-seed_iv-DCN-2000', '../logs/', max_epochs=500)
+    train(model, train_dataloader, validation_dataloader, 'all-DCN-2000', '../logs/', max_epochs=300)
 
     model_adv = Framework(encoders, latent_dim, 3, use_adversary=True)
-    train_adversarial(model_adv, train_dataloader, validation_dataloader, 'delete-seed-seed_iv-DCN-2000-adv', '../logs/',  0.05, max_epochs=500)      
+    train_adversarial(model_adv, train_dataloader, validation_dataloader, 'all-DCN-2000-adv', '../logs/',  0.05, max_epochs=300)      
 
 if __name__ == '__main__':
-    main()
+    run_four_sources()
--- examples/two_sources.py
+++ examples/two_sources.py
@@ -7,8 +7,9 @@ from pipeline_funcs import train, train_adversarial
 from torch.utils.data import DataLoader
 import platform
 import os
+import numpy as np
 
-def main():
+def run_two_sources():
     if  platform.system() == 'Darwin':
         path = '../../Datasets/private_encs/'
     else:
@@ -16,14 +17,13 @@ def main():
     
     train_datasource_files = [os.path.join(path,'train',f) for f in sorted(os.listdir(os.path.join(path, 'train'))) if f.endswith('.npz') and not('test' in f)]
     validation_datasource_files = [os.path.join(path,'val', f) for f in sorted(os.listdir(os.path.join(path, 'val'))) if f.endswith('.npz') and not('test' in f)]
+    train_datasource_files = [f for f in train_datasource_files if 'SEED' in f] # SEED and SEED-IV
+    print(train_datasource_files)
+    validation_datasource_files = [f for f in validation_datasource_files if 'SEED' in f] # SEED and SEED-IV
 
     latent_dim = 2000
-    encoders = [
-        architectures.EEGNetEncoder(channels=32, temporal_filters=F1, spatial_filters=D, pointwise_filters=F2, dropout_propability=0.25, latent_dim=latent_dim, use_constrained_conv=False), #DEAP
-        architectures.EEGNetEncoder(channels=14, temporal_filters=F1, spatial_filters=D, pointwise_filters=F2, dropout_propability=0.25, latent_dim=latent_dim, use_constrained_conv=False),  #DREAMER
-        architectures.EEGNetEncoder(channels=62, temporal_filters=F1, spatial_filters=D, pointwise_filters=F2, dropout_propability=0.25, latent_dim=latent_dim, use_constrained_conv=False), #SEED
-        architectures.EEGNetEncoder(channels=62, temporal_filters=F1, spatial_filters=D, pointwise_filters=F2, dropout_propability=0.25, latent_dim=latent_dim, use_constrained_conv=False) #SEED-IV
-    ]
+    encoders = [architectures.DeepConvNetEncoder(channels=62, latent_dim=latent_dim),
+                architectures.DeepConvNetEncoder(channels=62, latent_dim=latent_dim)]
     
     training_data = datasets.MultiSourceDataset(train_datasource_files)
     validation_data = datasets.MultiSourceDataset(validation_datasource_files)
@@ -32,10 +32,10 @@ def main():
     validation_dataloader = DataLoader(validation_data, batch_size=64, shuffle=False, num_workers=4, pin_memory=True)
 
     model = Framework(encoders, latent_dim, 3, use_adversary=False)
-    train(model, train_dataloader, validation_dataloader, 'seed-seed_iv-DCN-2000', '../logs/', max_epochs=100)
+    train(model, train_dataloader, validation_dataloader, 'seed-seed_iv-DCN-2000', '../logs/', max_epochs=500)
 
-    #model_adv = Framework(encoders, latent_dim, 3, use_adversary=True)
-    #train_adversarial(model_adv, train_dataloader, validation_dataloader, 'delete-seed-seed_iv-DCN-2000-adv', '../logs/',  0.05, max_epochs=500)      
+    model_adv = Framework(encoders, latent_dim, 3, use_adversary=True)
+    train_adversarial(model_adv, train_dataloader, validation_dataloader, 'seed-seed_iv-DCN-2000-adv', '../logs/',  0.05, max_epochs=500)      
 
 if __name__ == '__main__':
-    main()
+    run_two_sources()
