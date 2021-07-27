524ff16bb1baf549a2ce3e409bb8c8948c5c044e
Philipp Hallgarten <philipp.hallgarten@web.de>
2021-07-27 14:58:18 +0200

    changed scheduled runs

--- pipeline.py
+++ pipeline.py
@@ -71,5 +71,5 @@ if __name__ == '__main__':
     kappas = [0.05, 0.1, 0.2, 0.3, 0.4 ,0.5, 0.6, 0.7, 0.8, 0.9, 1., 2., 3., 4., 5., 10.]
 
     for kappa in kappas:
-        run_name = "delete-DCN-1111-50-mmd-%2.2f-v2"%(kappa)
+        run_name = "DCN-1111-50-mmd-%2.2f-v2"%(kappa)
         pipeline(['SEED', 'SEED_IV', 'DEAP', 'DREAMER'], architectures.DeepConvNetEncoder, 50, 'mmd', run_name, lam=kappa, logpath='../logs_v2/')
