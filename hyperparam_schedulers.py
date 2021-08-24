05f453b15a1ae3f66123855a8b8dcb0861773413
Philipp Hallgarten <philipp.hallgarten@web.de>
2021-08-24 11:25:16 +0200

    changed implementation from function to class

--- hyperparam_schedulers.py
+++ hyperparam_schedulers.py
@@ -1,40 +1,82 @@
 import numpy as np
 
-def constant_schedule(epoch, value=1):
-    return value
+class constant_schedule():
+    def __init__(self, value=1):
+        self.value = 1
 
-def constant_linear_schedule(epoch, start_epoch=0, start_value=0, step_value=0.5):
-    if epoch <= start_epoch:
-        return start_value
-    else:
-        return start_value + (epoch-start_epoch)*step_value
+    def __getitem__(self, epoch):
+        return self.value
 
-def linear_constant_schedule(epoch, step_value=0.1, stop_epoch=10):
-    if epoch < stop_epoch:
-        return epoch * step_value
-    else:
-        return stop_epoch * step_value
+    def __str__(self):
+        return f"C-{self.value}"
 
-def constant_linear_constant_schedule(epoch, start_epoch=0, start_value=0, step_value=0.1, stop_epoch=1):
-    if epoch < start_epoch:
-        return start_value
-    elif epoch < stop_epoch:
-        return start_value + (epoch-start_epoch)*step_value
-    else:
-        return start_value + (stop_epoch-start_epoch)*step_value
+class constant_linear_schedule():
+    def __init__(self, start_epoch=0, start_value=0, step_value=0.5):
+        self.start_epoch = start_epoch
+        self.start_value = start_value
+        self.step_value = step_value
+    
+    def __getitem__(self, epoch):
+        if epoch <= self.start_epoch:
+            return self.start_value
+        else:
+            return self.start_value + (epoch-self.start_epoch)*self.step_value
+
+    def __str__(self):
+        return f"CL-{self.start_epoch}-{self.start_value}-{self.step_value}"
+
+
+class linear_constant_schedule():
+    def __init__(self, step_value=0.1, stop_epoch=10):
+        self.step_value = step_value
+        self.stop_epoch = stop_epoch
+
+    def __getitem__(self,epoch):
+        if epoch < self.stop_epoch:
+            return epoch * self.step_value
+        else:
+            return self.stop_epoch * self.step_value
+
+    def __str__(self):
+        return f"LC-{self.step_value}-{self.stop_epoch}"
+
+class constant_linear_constant_schedule():
+    def __init__(self, start_epoch=0, start_value=0, step_value=0.1, stop_epoch=1):
+        self.start_epoch = start_epoch
+        self.start_value = start_value
+        self.step_value = step_value
+        self.stop_epoch = stop_epoch
+
+    def __getitem__(self, epoch):
+        if epoch < self.start_epoch:
+            return self.start_value
+        elif epoch < self.stop_epoch:
+            return self.start_value + (epoch-self.start_epoch)*self.step_value
+        else:
+            return self.start_value + (self.stop_epoch-self.start_epoch)*self.step_value
+    
+    def __str__(self):
+        return f"CLC-{self.start_epoch}-{self.start_value}-{self.step_value}-{self.stop_epoch}"
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     epochs = np.arange(20)
+
+    c = constant_schedule(value = 1)
+    cl = constant_linear_schedule(2, 0.2, 0.1)
+    lc = linear_constant_schedule(0.5, 10)
+    clc = constant_linear_constant_schedule(4, 0.4, 0.05, 17)
+
+
     constant_values = list()
     constant_linear_values = list()
     linear_constant_values = list()
     constant_linear_constant_values = list()
     for epoch in epochs:
-        constant_values.append(constant_schedule(epoch, 1))
-        constant_linear_values.append(constant_linear_schedule(epoch, 2, 0.2, 0.1))
-        linear_constant_values.append(linear_constant_schedule(epoch, 0.5, 10))
-        constant_linear_constant_values.append(constant_linear_constant_schedule(epoch, 4, 0.4, 0.05, 17))
+        constant_values.append(c[epoch])
+        constant_linear_values.append(cl[epoch])
+        linear_constant_values.append(lc[epoch])
+        constant_linear_constant_values.append(clc[epoch])
     plt.plot(epochs, constant_values, label='Constant Schedule')
     plt.plot(epochs, constant_linear_values, label='Constant Linear Schedule')
     plt.plot(epochs, linear_constant_values, label='Linear Constant Schedule')
